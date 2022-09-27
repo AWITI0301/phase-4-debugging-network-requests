d6ef6c7008982d4aa7fa4b2c7a63ec54fbdafbe3
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-27 22:59:54 +0300

    commit tree 0b4fbc78ef20b033c364fc6f91c694e2ee7f8748

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,18 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    # byebug
+    # params
+    # #<ActionController::Parameters {"title"=>"", "year"=>2021, "length"=>"0","director"=>"", ...> 
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end 
+
 end
